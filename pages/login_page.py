dbdcdc29dd44c1540ed3173b2962fc265a6f1d8c
OlgaBeznutrova <olga.beznutrova@gmail.com>
2021-06-22 16:43:44 +1200

    Added methods

--- pages/login_page.py
+++ pages/login_page.py
@@ -2,7 +2,7 @@ from .base_page import BasePage
 from .locators import LoginPageLocators
 
 
-class LoginPage(BasePage):  # страница с логином
+class LoginPage(BasePage):
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
@@ -16,3 +16,11 @@ class LoginPage(BasePage):  # страница с логином
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented on login_page"
+
+    def register_new_user(self, email, password):
+        self.browser.find_element(*LoginPageLocators.INPUT_EMAIL).send_keys(email)
+        self.browser.find_element(*LoginPageLocators.INPUT_PASSWORD_1).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.INPUT_PASSWORD_2).send_keys(password)
+        self.browser.find_element(*LoginPageLocators.BUTTON_REGISTRATION).click()
+
+
