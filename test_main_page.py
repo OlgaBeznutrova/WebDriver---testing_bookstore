286ab8b3c80512be745d2574db353ab28c0c73ad
OlgaBeznutrova <olga.beznutrova@gmail.com>
2021-06-22 16:45:22 +1200

    Created testing class

--- test_main_page.py
+++ test_main_page.py
@@ -1,18 +1,33 @@
+import pytest
 from pages.login_page import LoginPage
 from pages.main_page import MainPage
+from pages.basket_page import BasketPage
 
 
-def test_guest_should_see_login_link(browser):
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
+
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()
-
-
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()  # кликнет по ссылке логина
-    login_page = LoginPage(browser, browser.current_url)  # тут же создали экземпляр LoginPage
-    login_page.should_be_login_page()  # и воспользовались методом LoginPage класса, т.е. фактически тестируется сам переход от одной страницы к другой
+    page.should_be_basket_link()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_not_be_product_in_basket()
+    basket_page.should_not_be_disappeared_message_about_empty_basket()
